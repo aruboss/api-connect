c80e5df124cc3419747db3921877ddbc6085c8ef
aruboss <minhhieu1096@gmail.com>
2018-10-07 08:31:10 +0700

    tich hop router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/Menu/Menu';
 import ProductList from './components/ProductList/ProductList';
+import routes from './routes';
+import { Switch, Route } from 'react-router-dom'; 
 
 class App extends Component {
   render() {
@@ -11,18 +13,23 @@ class App extends Component {
 
         <div className="container">
           <div className="row">
-            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            {/* <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
               <button type="button" className="btn btn-info mb-10">
                 Thêm Sản Phẩm
               </button>
               <ProductList/>
 
-            </div>
+            </div>  */}
+            {this.showContentMenus(routes)}
           </div>
         </div>
-
       </div>
     );
+  }
+  showContentMenus = (routes) => {
+    var result = null;
+
+    return <Switch>{result}</Switch>
   }
 }
 

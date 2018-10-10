690dd9fc364b1a23b511b2202baf01057d005424
aruboss <minhhieu1096@gmail.com>
2018-10-10 23:40:03 +0700

    them router

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,29 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const menus = [
+    {
+        name: 'Trang Chủ',
+        to: '/',
+        exact: true
+    },
+    {
+        name: 'Quản Lý Dản Phẩm',
+        to: '/product-list',
+        exact: false
+    }
+]
+
+const MenuLink = ({ label, to, activeOnlyWhenExact }) => (
+    <Route
+        path={to}
+        exact={activeOnlyWhenExact}
+        children={({ match }) => {
+            var active = match ? 'active' : '';
+        }
+        }
+    />
+);
 
 class Menu extends Component {
     render() {

0a1c0372a409a98107aa418b951339f70f787953
aruboss <minhhieu1096@gmail.com>
2018-10-18 22:39:04 +0700

    Sử Dụng Axios Gọi API 7:00

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -5,7 +5,8 @@ import { connect } from 'react-redux';
 
 class ProductListPage extends Component {
     render() {
-        var { products } = this.props;
+        // var { products } = this.props;
+        var products = [];
         return (
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <button type="button" className="btn btn-info mb-10">

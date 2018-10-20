e6cc3af4e61634949a54a29437347e655b1299a9
aruboss <minhhieu1096@gmail.com>
2018-10-20 21:38:28 +0700

    Sử Dụng Axios Gọi API

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import ProductList from './../../components/ProductList/ProductList'
 import ProductItem from './../../components/ProductItem/ProductItem'
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 class ProductListPage extends Component {
     render() {
-        // var { products } = this.props;
         var products = [];
+        axios({
+            method: 'GET',
+            url: 'http://localhost:3000/products',
+            data: null
+        }).then(res => {
+            console.log(res);
+            products = res.data;
+        }).catch(err => {
+            console.log(err);
+        });
         return (
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <Link to="/product/add" type="button" className="btn btn-info mb-10">
+                <button type="button" className="btn btn-info mb-10">
                     Thêm Sản Phẩm
-              </Link>
+              </button>
                 <ProductList >
                     {this.showProducts(products)}
                 </ProductList>
